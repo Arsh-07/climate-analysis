0a8ef76876bf35eda15ae4ef66a208515d91735f
Arshad <a.emmambux@soton.ac.uk>
2015-12-16 11:39:08 +0000

    Developer A added a line

--- rainfall_conversion.py
+++ rainfall_conversion.py
@@ -9,5 +9,5 @@ def inches_to_mm(inches)
     mm=inches*25.4
     return mm
 
-#TODO(Developer B): Add function to convert from inches to cm
+#TODO(Developer B): Adding function to convert from inches to cm
 
