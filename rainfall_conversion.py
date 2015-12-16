42d72a9bbb604f2d352e72bb96394f4c31587c4a
Arshad <a.emmambux@soton.ac.uk>
2015-12-16 11:34:38 +0000

    Developer A added a line

--- rainfall_conversion.py
+++ rainfall_conversion.py
@@ -1,10 +1,13 @@
 """ A library to perform rainfall unit conversions"""
+
 def inches_to_mm(inches)
     """Convert inches to milimeters.
+
     Arguments:
     inches  --  the rainfall inches
     """
     mm=inches*25.4
     return mm
 
+#TODO(Developer B): Add function to convert from inches to cm
 
